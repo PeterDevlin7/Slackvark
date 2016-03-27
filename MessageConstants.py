a560d9919544d6d708e4735673164cd5499fee4e
Nuri Ra <nra1@binghamton.edu>
2016-03-26 23:46:26 -0400

    migrated bots message prompts

---
+++ MessageConstants.py
@@ -0,0 +1,22 @@
+"""
+Menu Options
+"""
+# hello
+MENU = "Hi! I am a test build of Slackvark. Please choose from these options:\n" \
+        "1. Exit Menu\n"\
+        "2. Send a direct message\n"\
+        "3. Create a new group\n"\
+        "4. Message the Legal Slack"
+
+MENU_OPTION_1 = "Good-bye!"
+MENU_OPTION_3 = "Who would you like to invite?"
+MENU_OPTION_4 = "What should the channel name be?"
+
+CREATED_CHANNEL = "Hello again! I've created this channel for you."
+PROMPT_CHANNEL = "What should the channel name be?"
+
+
+"""
+General Messages
+"""
+JOIN_CHANNEL = "Hello there, welcome to the channel"
